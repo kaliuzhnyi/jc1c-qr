24b4713e55e6153ad2de3732b5395cdfe7451bba
Ivan Kaliuzhnyi <kalyuzhny.ivan@gmail.com>
2021-10-04 08:51:02 +0300

    Update dependencies - jc1c(1.0.7)

--- src/main/java/org/jc1c/qr/App.java
+++ src/main/java/org/jc1c/qr/App.java
@@ -6,34 +6,12 @@ import java.io.*;
 
 public class App {
 
-    public static Integer QRCodeWidth = 500;
-    public static Integer QRCodeHeight = 500;
-
     public static void main(String[] args) throws IOException {
 
-        String hostname = (args.length >= 1 && !args[0].isBlank())
-                ? args[0]
-                : null;
-
-        Integer port = (args.length >= 2 && !args[1].isBlank())
-                ? Integer.valueOf(args[1])
-                : null;
-
-        String apiKey = (args.length >= 3 && !args[2].isBlank())
-                ? args[2]
-                : null;
-
-        Long limitTime = (args.length >= 4 && !args[3].isBlank())
-                ? Long.valueOf(args[3])
-                : null;
-
         JServer.builder()
-                .withHostname(hostname)
-                .withPort(port)
-                .withApiKey(apiKey)
+                .withArgs(args)
                 .withBacklog(3)
                 .withThreadPool(3)
-                .withHandlersProcessingTimeController(limitTime)
                 .withHandlersController(AppHandlers.class)
                 .build()
                 .start();
